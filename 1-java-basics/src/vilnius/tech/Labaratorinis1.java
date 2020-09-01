9478a01ed8dfdc3e5a7f01d78d4eeede5c9ba714
Aurimas Šakalys <aurimas.sakalys@stud.vgtu.lt>
2020-09-01 21:26:36 +0300

    Task 2

--- 1-java-basics/src/vilnius/tech/Labaratorinis1.java
+++ 1-java-basics/src/vilnius/tech/Labaratorinis1.java
@@ -1,8 +1,12 @@
 package vilnius.tech;
 
+/**
+ * @author Aurimas Šakalys PRIf-18/2 20185388
+ * Klasė skirta 1 labaratorinio darbo atlikimui.
+ */
 public class Labaratorinis1 {
 
     public static void main(String[] args) {
-        
+
     }
 }
