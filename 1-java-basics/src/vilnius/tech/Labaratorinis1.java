36d03ef4f6ce1ff2492db4a4aa4b549d6eb327f5
Mark C. David <aurimas.sakalys@stud.vgtu.lt>
2020-09-08 12:39:09 +0300

    Translate to Lithuanian

--- 1-java-basics/src/vilnius/tech/Labaratorinis1.java
+++ 1-java-basics/src/vilnius/tech/Labaratorinis1.java
@@ -42,9 +42,9 @@ public class Labaratorinis1 {
         Float[] rowAverages = new Float[nameLength];
         Float[] columnAverages = new Float[surnameLength];
 
-        for(int row = 0; row < nameLength; row++) {
-            for(int column = 0; column < surnameLength; column++){
-                if(rowAverages[row] == null){
+        for (int row = 0; row < nameLength; row++) {
+            for (int column = 0; column < surnameLength; column++) {
+                if (rowAverages[row] == null) {
                     rowAverages[row] = 0f;
                 }
                 rowAverages[row] += array[row][column];
@@ -52,9 +52,9 @@ public class Labaratorinis1 {
             rowAverages[row] /= surnameLength;
         }
 
-        for(int column = 0; column < surnameLength; column++) {
-            for(int row = 0; row < nameLength; row++){
-                if(columnAverages[row] == null){
+        for (int column = 0; column < surnameLength; column++) {
+            for (int row = 0; row < nameLength; row++) {
+                if (columnAverages[row] == null) {
                     columnAverages[row] = 0f;
                 }
                 columnAverages[column] += array[row][column];
@@ -65,46 +65,46 @@ public class Labaratorinis1 {
         final DecimalFormat decimalFormat = new DecimalFormat("##.0000");
         decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
 
-	System.out.println("Rows: ");
+        System.out.println("Rows: ");
         PrintArray(rowAverages, decimalFormat);
-	System.out.println("Columns: ");
+        System.out.println("Columns: ");
         PrintArray(columnAverages, decimalFormat);
 
         // Užduotis #6 - skaičiuojame skaičių kiekį kiekvienoje eilutėje kurių reikšmė didesnis nei tos eilutės vidurkis
-        for(int row = 0; row < nameLength; row++) {
+        for (int row = 0; row < nameLength; row++) {
             int count = 0;
-            for(int column = 0; column < surnameLength; column++){
-                if(array[row][column] > rowAverages[row]){
+            for (int column = 0; column < surnameLength; column++) {
+                if (array[row][column] > rowAverages[row]) {
                     count++;
                 }
             }
             System.out.println("There are " + count + " numbers in row " + row + " that are larger than the average");
         }
-	
-	// Task #7 - find max number in 2D array excluding a-th row and column
-	System.out.println("Max value excluding row " + min + " and column " + min + " is " + MaxExcluding(array, min, min));
-	
-	// Task #8 - print a-th row values sorted without mutating the array
-	Integer[] arrayCopy = array[min].clone();
-	Arrays.sort(arrayCopy);
-	PrintArray(arrayCopy, NumberFormat.getInstance());	
-	
-	// Task #9 - find index of min value in columnAverage array and print the min value in the column of said index.
-	int minIndex = IndexOfMin(columnAverages);
-	int minValue = array[0][minIndex];
-	for(int i = 1; i < nameLength; i++) {
-		if(array[i][minIndex] < minValue) {
-			minValue = array[i][minIndex];
-		}
-	}	
-	System.out.println(minValue);
+
+        // Užduotis #7 - randame didžiausią reikšmę masyve, 'išmetus' a-tąją eilutę ir stulpelį
+        System.out.println("Max value excluding row " + min + " and column " + min + " is " + MaxExcluding(array, min, min));
+
+        // Užduotis #8 - išspausdinti a-tosios eilutės reikšmes jos nemutuojant
+        Integer[] arrayCopy = array[min].clone();
+        Arrays.sort(arrayCopy);
+        PrintArray(arrayCopy, NumberFormat.getInstance());
+
+        // Užduotis #9 - rasti mažiausio vidurkio stulpeliuose indeksą ir rasti mažiausią reikšmę tame stulpelyje;
+        int minIndex = IndexOfMin(columnAverages);
+        int minValue = array[0][minIndex];
+        for (int i = 1; i < nameLength; i++) {
+            if (array[i][minIndex] < minValue) {
+                minValue = array[i][minIndex];
+            }
+        }
+        System.out.println(minValue);
 
     }
 
     public static int VowelCount(String target) {
         int total = 0;
         for (char c : target.toLowerCase().toCharArray()) {
-            if(vowels.indexOf(c) >= 0){
+            if (vowels.indexOf(c) >= 0) {
                 total++;
             }
         }
@@ -112,31 +112,30 @@ public class Labaratorinis1 {
     }
 
     public static int IndexOfMin(Float[] array) {
-	Float min = array[0];
-	int index = 0;
-	for(int i = 1; i < array.length; i++){
-		if(array[i] < min) {
-			min = array[i];
-			index = i;
-		}
-	}
-	return index;
+        Float min = array[0];
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        return index;
     }
 
     public static Integer MaxExcluding(Integer[][] array, int rowExclude, int columnExclude) {
-    	Integer max = -1;	
-	for(int row = 0; row < array.length; row++){
-    	    for(int col = 0; col < array[row].length; col++){
-		if(row == rowExclude || col == columnExclude) {
-			continue;
-		}
-		if(array[row][col] > max){
-			max = array[row][col];
-		}		
-			
-	    }	
-    	}
-	return max;
+        Integer max = -1;
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++) {
+                if (row == rowExclude || col == columnExclude) {
+                    continue;
+                }
+                if (array[row][col] > max) {
+                    max = array[row][col];
+                }
+            }
+        }
+        return max;
     }
 
     public static <T> void PrintArray(T[] array, Format format) {
@@ -148,8 +147,8 @@ public class Labaratorinis1 {
 
     public static <T> void Print2DArray(T[][] array, boolean showIndexes, Format format) {
         for (int i = 0; i < array.length; i++) {
-            if(showIndexes) {
-                if(i == 0) PrintArrayColumnIndexes(array[i].length);
+            if (showIndexes) {
+                if (i == 0) PrintArrayColumnIndexes(array[i].length);
                 PrintArrayRowIndex(i);
             }
             PrintArray(array[i], format);
