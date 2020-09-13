6829fb449976e06fd15cd5337e535c914ae20cbb
Aurimas Šakalys <aurimas.sakalys@stud.vgtu.lt>
2020-09-13 09:20:30 +0300

    User text switched to text area.

--- op-3-chatroom/op-3-chatroom-client/src/ChatRoomForm.java
+++ op-3-chatroom/op-3-chatroom-client/src/ChatRoomForm.java
@@ -3,10 +3,10 @@ import javax.swing.*;
 public class ChatRoomForm {
     public JPanel mainPanel;
     private JEditorPane chatTextPane;
-    private JTextField userText;
     private JButton submitButton;
     private JList loggedInList;
     private JButton logOutButton;
+    private JTextArea userMessageField;
 
     public ChatRoomForm() {
         chatTextPane.setText("[Mark]: Hello\n" +
